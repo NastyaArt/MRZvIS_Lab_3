160d08ed1bf05868f0c20f1c08d7b27a7b38ab4a
NastyaArt <artemchuk.nastya@gmail.com>
2018-01-04 19:19:58 +0300

    add read from file

--- biassociativememory.cpp
+++ biassociativememory.cpp
@@ -3,5 +3,118 @@
 BiAssociativeMemory::BiAssociativeMemory()
 {
 
+
+    ReadFromFileImg_Ass("test.txt");
+
+    PrintImg(imgX);
+
 }
 
+void BiAssociativeMemory::Educate()
+{
+
+}
+
+void BiAssociativeMemory::Recognize()
+{
+
+}
+
+void BiAssociativeMemory::PrintImg(string img)
+{
+    for (int i = 0; i < n; i+=p)
+    {
+        for (int j = 0; j < p; j++)
+        {
+            cout << img[i+j];
+        }
+        cout << endl;
+    }
+}
+
+void BiAssociativeMemory::ReadFromFileImg_Ass(char* path)
+{
+
+    string buf, X_, Y_;
+
+    ifstream file(path);
+    if (!file.is_open())
+    {
+        cout << "The file can not be opened!\n";
+        return;
+    }
+    file >> n;
+    file >> p;
+
+    if (n%p!=0)
+    {
+        cout << "Error! The file contains incorrect data!" << endl;
+        return;
+    }
+
+    getline(file,  buf);
+
+    for (int i = 0; i < buf.length() || i < n + p; i++)
+    {
+        if (buf[i]!= DOT && buf[i]!= GRID)
+        {
+            cout << "Error! The file contains incorrect data!" << endl;
+            return;
+        }
+        if (i < n)
+        {
+            X_.push_back(buf[i]);
+        }
+        else
+        {
+            Y_.push_back(buf[i]);
+        }
+
+    }
+    file.close();
+
+    imgX = X_;
+    imgY = Y_;
+
+}
+
+mat BiAssociativeMemory::VecToMat(vec v)
+{
+    mat X;
+
+    return X;
+
+}
+
+vec BiAssociativeMemory::StringToVec(string str)
+{
+    vec Y;
+
+    return Y;
+}
+
+string BiAssociativeMemory::MatToImage(mat X)
+{
+    string img;
+
+    return img;
+
+}
+
+mat BiAssociativeMemory::ReadFromFileRecognImage(char* path)
+{
+    mat X;
+
+    return X;
+}
+
+double BiAssociativeMemory::CountL()
+{
+    int m = min(n, p);
+    return (m/(4*log(m)));
+}
+
+void BiAssociativeMemory::AddX_Y(mat X, vec Y)
+{
+
+}
--- biassociativememory.h
+++ biassociativememory.h
@@ -5,6 +5,7 @@
 #ifndef BIASSOCIATIVEMEMORY_H
 #define BIASSOCIATIVEMEMORY_H
 
+#include <fstream>
 #include <iostream>
 #include <armadillo>
 /**
@@ -16,10 +17,41 @@
 using namespace std;
 using namespace arma;
 
+#define DOT '.'
+#define GRID '#'
+
 class BiAssociativeMemory
 {
 public:
     BiAssociativeMemory();
+    void Educate();
+    void Recognize();
+
+private:
+    int n;  //размер образа
+    int p;  //размер ассоциации
+    int m;  //количество строк в матрице образа m*p=m
+    int Q;  //количество пар образ - ассоциация, на которую обучена сеть
+    double L;  //емкость сети
+    mat W;  //веса первого слоя p*n
+    mat W_; //веса второго слоя n*p
+
+    vector<mat> Xk; //список образов
+    vector<vec> Yk; //список ассоциаций
+
+    string imgX;    //строка текущего образа
+    string imgY;    //строка текущей ассоциации
+
+    mat X_; //подаваемый искаженный образ
+
+    void ReadFromFileImg_Ass(char* path);   //считать пару образ - ассоциацию
+    mat ReadFromFileRecognImage(char* path);   //считать образ для распознавания
+    double CountL();  //почитать L
+    mat VecToMat(vec v);
+    vec StringToVec(string str);
+    string MatToImage(mat X);
+    void PrintImg(string img);
+    void AddX_Y(mat X, vec Y);  //добавить пару образ - ассоциация
 };
 
 #endif // BIASSOCIATIVEMEMORY_H
